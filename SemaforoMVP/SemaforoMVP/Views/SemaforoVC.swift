29e43209316ade557b22454668017bf6919d7768
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-02-27 17:21:28 +0100

    Second Commit

---
+++ SemaforoMVP/SemaforoMVP/Views/SemaforoVC.swift
@@ -0,0 +1,30 @@
+//
+//  SemaforoVC.swift
+//  SemaforoMVP
+//
+//  Created by Miguel on 27/02/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class SemaforoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
